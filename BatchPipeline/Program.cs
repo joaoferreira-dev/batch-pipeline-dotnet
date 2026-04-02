961bcb730744e020e694727dd19bbc7cb5b93053
Joao Ferreira <joaobizuca10@gmail.com>
2026-04-02 20:08:28 -0300

    feat: creating initial version of file processor worker

--- BatchPipeline/Program.cs
+++ BatchPipeline/Program.cs
@@ -1,7 +1,12 @@
-using BatchPipeline;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddSingleton<FileProcessor>();
+        services.AddHostedService<FilePipelineWorker>();
+    })
+    .Build();
 
-var host = builder.Build();
-host.Run();
+await host.RunAsync();
